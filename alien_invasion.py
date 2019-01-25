3c0f13d107b789b909160ec39c69f4b66e5974a1
zippermonkey <1960727314@qq.com>
2019-01-25 15:46:41 +0800

    为屏幕添加一艘飞船

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from setting import Settings
+from ship import Ship
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -11,6 +12,9 @@ def run_game():
     screen = pygame.display.set_mode(
             (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     # 开始游戏的主循环
     while True:
@@ -22,6 +26,8 @@ def run_game():
 
         # 每次循环都重绘屏幕
         screen.fill(ai_settings.bg_color)
+        # 加入飞船
+        ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
