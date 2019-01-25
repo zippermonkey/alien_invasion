e15c8d91a6131ed615ec687074e4a3e9d30cbf85
zippermonkey <1960727314@qq.com>
2019-01-25 21:04:35 +0800

    更新速度

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,16 +4,17 @@ from setting import Settings
 from ship import Ship
 import game_functions as gf
 
+
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-            (ai_settings.screen_width,ai_settings.screen_height))
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # 开始游戏的主循环
     while True:
@@ -21,7 +22,7 @@ def run_game():
         gf.check_events(ship)
         ship.update()
         # 更新屏幕
-        gf.update_screen(ai_settings,screen,ship)
-        
-    
+        gf.update_screen(ai_settings, screen, ship)
+
+
 run_game()
--- ship.py
+++ ship.py
@@ -1,10 +1,12 @@
 import pygame
 
+
 class Ship():
 
-    def __init__(self,screen):
+    def __init__(self, ai_settings, screen):
         """初始化飞船并设置其初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # 加载飞船并获取其外接矩形
         self.image = pygame.image.load('images/ship.bmp')
@@ -15,6 +17,9 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # 在飞船的属性center中存储小数值
+        self.center = float(self.rect.centerx)
+
         # 移动标志
         self.moving_right = False
         self.moving_left = False
@@ -22,11 +27,14 @@ class Ship():
     def update(self):
         """根据移动标志调整飞船的位置"""
         if self.moving_right:
-            self.rect.centerx += 1
-        
+            self.center += self.ai_settings.ship_speed_factor
+
         if self.moving_left:
-            self.rect.centerx -= 1
-    
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # 根据self.center更新rect对象
+        self.rect.centerx = self.center
+
     def blitme(self):
         """在指定位置绘制飞船"""
-        self.screen.blit(self.image,self.rect)
+        self.screen.blit(self.image, self.rect)
