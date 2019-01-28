7672b5a4cf62aeed9f063e4c7ca57edda31cfea9
zippermonkey <1960727314@qq.com>
2019-01-28 16:04:07 +0800

    添加子弹设置

--- setting.py
+++ setting.py
@@ -7,6 +7,12 @@ class Settings():
         self.screen_width = 1280
         self.screen_height = 768
         # 背景颜色设置
-        self.bg_color = (230,230,230)
+        self.bg_color = (230, 230, 230)
         # 速度设置
         self.ship_speed_factor = 1.5
+
+        # 添加子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
