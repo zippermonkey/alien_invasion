fdb1d3e37eb7de190b708d201522c732d2e8317a
zippermonkey <1960727314@qq.com>
2019-02-03 23:31:01 +0800

    提高游戏等级

--- game_functions.py
+++ game_functions.py
@@ -54,6 +54,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """在玩家单击play时开始游戏"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        # 重置游戏设置
+        ai_settings.initialize_dynamic_settings()
+
         # 隐藏光标
         pygame.mouse.set_visible(False)
         # 重置游戏统计信息
@@ -108,6 +111,7 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     if len(aliens) == 0:
         # 删除现有的子弹并新建一批外星人
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 
--- setting.py
+++ setting.py
@@ -9,16 +9,31 @@ class Settings():
         # 背景颜色设置
         self.bg_color = (230, 230, 230)
         # 飞船设置
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
         # 外星人速度设置
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        # fleet_direction 为1表示右移 -1表示左移
-        self.fleet_direction = 1
-
         # 添加子弹设置
-        self.bullet_speed_factor = 1
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # 以什么样的速度加快游戏节奏
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+    
+    def initialize_dynamic_settings(self):
+        """初始化随游戏进行变化的属性"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        # fleet_direction 为1表示右移 -1表示左移
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """提高速度设置"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
