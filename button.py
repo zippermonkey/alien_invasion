6af5acfb3fef48824bb7a5cd8bc3eb7ee9038ad6
zippermonkey <1960727314@qq.com>
2019-02-03 11:18:06 +0800

    绘制play按钮

--- button.py
+++ button.py
@@ -24,3 +24,8 @@ class Button():
         self.msg_image = self.font.render(msg, True, self.text_color, self.button_color)
         self.msg_image_rect = self.msg_image.get_rect()
         self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self):
+        # 绘制一个用颜色填充的按钮 ，在绘制文本
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
